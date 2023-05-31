fafec3252ddc0e4fa446ddd034c944da5abe6196
hyoaru <jjcabreraaaa@gmail.com>
2023-05-31 20:14:01 +0800

    [fix] footer mobile view

--- client/src/base_components/Footer.jsx
+++ client/src/base_components/Footer.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
     return (
         <>
-            <div className="container mx-auto">
+            <div className="container mx-auto px-8">
                 <div className="flex">
                     <small className="mx-auto opacity-40">
-                        © 2023 A Not-So-Public Public Board. A Course Requirement in Algorithms and Complexity 101. {" "}
+                        © 2023 
+                        <Link to={"admin"} className="hover:text-primary"> A Not-So-Public Public Board. {" "}</Link>
+                        A Course Requirement in Algorithms and Complexity 101. {" "}
                         <a className="underline decoration-solid" href="https://github.com/hyoaru">Hyoaru</a>
                         . All Rights Reserved.
                     </small>
